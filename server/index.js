02e36f73874421135e21e154182ab6ffc70f306d
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-03-23 12:27:00 -0400

    added emoji feature

--- server/index.js
+++ server/index.js
@@ -18,25 +18,32 @@ io.on('connection', (socket)=>{
         
         if(error) return callback(error);
 
+        // adding a user to a particular room
+        socket.join(user.room);
+
         // letting the user who joined know that he/she has joined the room
         socket.emit('message', { user: 'admin', text: `${user.name} welcome to the room ${user.room}`});
         // letting everyone else in the room know that a new user has joined
         socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!`});
 
-        // adding a user to a particular room
-        socket.join(user.room);
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInroom(user.room)});
 
         callback();
     });
 
-    socket.on('sendMessage',(message, callback)=>{
+    socket.on('sendMessage', (message, callback) => {
         const user = getUser(socket.id);
+        console.log(user);
         io.to(user.room).emit('message',{ user: user.name, text: message });
+        io.to(user.room).emit('roomData',{ room: user.name, users: getUsersInroom(user.room) });
         callback();
     });
 
     socket.on('disconnect',()=>{
-        console.log('User has left!');
+        const user = removeUser(socket.io);
+        if(user){
+            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.`});
+        }
     });
 });
 
